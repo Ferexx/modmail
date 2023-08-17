9b423e87cf10243654b509a3823ba92a27e6ba3e
Ferex <jack.price99@hotmail.com>
2023-08-17 15:55:12 +0100

    Security

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,20 +1,20 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const fs = require('node:fs');
+const config = require('../config.json')
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Place your client and guild ids here
-const clientId = '1013498455248883802';
-const guildId = '1013505875979276400';
+const clientId = config.clientId;
+const guildId = config.inboxServerId;
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken('MTAxMzQ5ODQ1NTI0ODg4MzgwMg.GDCfKB.XuXcOn6EunphCnSN2b9X4vKvh2OCibd9XrZz54');
+const rest = new REST({ version: '10' }).setToken(config.token);
 
 (async () => {
 	try {
